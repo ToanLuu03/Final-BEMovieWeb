521e65ccbe7e9bf451fdddd6d10a12a48a76fae0
Luu Truong Toan <luutruongtoanltt@gmail.com>
2025-03-16 20:04:28 +0700

    no auth theater

--- routes/theaterRoutes.js
+++ routes/theaterRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const theaterController = require('../controllers/theaterController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.get('/search/get_theaters', authMiddleware, theaterController.getTheaters);
-router.get('/get_theater_detail/:id', authMiddleware, theaterController.getTheaterById);
-router.get('/get_all_theater', authMiddleware, theaterController.getAllTheaters);
+router.get('/search/get_theaters', theaterController.getTheaters);
+router.get('/get_theater_detail/:id', theaterController.getTheaterById);
+router.get('/get_all_theater', theaterController.getAllTheaters);
 
 module.exports = router;
